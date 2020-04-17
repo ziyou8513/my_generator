6007645ad1522cb33411d263fc9faaf226859b87
duanxufei <duanxufei@20km.net>
2020-04-17 10:14:37 +0800

    CR-0417:update page template

--- src/templates/Page.java
+++ src/templates/Page.java
@@ -7,24 +7,24 @@ public class Page {
     public static final String TEMPLATE = "package ${modelHome};\n" +
             "\n" +
             "import java.io.Serializable;\n" +
+            "import java.util.ArrayList;\n" +
+            "import java.util.Collections;\n" +
             "import java.util.List;\n" +
             "\n" +
             "public class Page<T> implements Serializable {\n" +
-            "    //current page data\n" +
-            "    private List<T> data;\n" +
-            "    //total record num\n" +
+            "    private List<? extends T> data;\n" +
             "    private Integer totalNum;\n" +
             "\n" +
-            "    public Page(List<T> data, Integer totalNum) {\n" +
+            "    public Page(List<? extends T> data, Integer totalNum) {\n" +
             "        this.data = data;\n" +
             "        this.totalNum = totalNum;\n" +
             "    }\n" +
             "\n" +
-            "    public List<T> getData() {\n" +
+            "    public List<? extends T> getData() {\n" +
             "        return data;\n" +
             "    }\n" +
             "\n" +
-            "    public void setData(List<T> data) {\n" +
+            "    public void setData(List<? extends T> data) {\n" +
             "        this.data = data;\n" +
             "    }\n" +
             "\n" +
@@ -34,6 +34,21 @@ public class Page {
             "\n" +
             "    public void setTotalNum(Integer totalNum) {\n" +
             "        this.totalNum = totalNum;\n" +
+            "    }\n" +
+            "\n" +
+            "    public static <T> Page<T> makeSinglePage(T t) {\n" +
+            "        if (t == null) {\n" +
+            "            return new Page<>(new ArrayList<>(), 0);\n" +
+            "        }\n" +
+            "        List<T> list = Collections.singletonList(t);\n" +
+            "        return new Page<>(list, 1);\n" +
+            "    }\n" +
+            "\n" +
+            "    public static <T> Page<T> makeListPage(List<T> tList) {\n" +
+            "        if (tList == null) {\n" +
+            "            return new Page<>(new ArrayList<>(), 0);\n" +
+            "        }\n" +
+            "        return new Page<>(tList, tList.size());\n" +
             "    }\n" +
             "}";
 }
